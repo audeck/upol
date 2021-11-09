a98c5618b91aa143ce8e45083dfdb5a4561fbb6d
audeck <55060124+audeck@users.noreply.github.com>
2021-11-09 03:07:11 +0100

    Create 7.lisp

---
+++ pp1/7.lisp
@@ -0,0 +1,52 @@
+(defun plist-remove (plist prop)
+    (cond ((null plist) nil)
+          ((eql (car plist) prop) (plist-remove (cddr plist) prop))
+          (t (cons (car plist) (cons (cadr plist) (plist-remove (cddr plist) prop))))))
+
+(defun plist-add (plist prop val)
+    (cond ((null plist) (cons prop (cons val nil)))
+          ((eql (car plist) prop) (cons prop (cons val (cddr plist))))
+          (t (cons (car plist) (cons (cadr plist) (plist-add (cddr plist) prop val))))))
+
+// (tree-node 5 (list (tree-node 2 (list (tree-node 1 nil) (tree-node (tree-node 3 nil) (list (tree-node 4 nil))))) (tree-node 7 (list (tree-node 6 nil) (tree-node 8 nil)))))
+
+// (setf tree (tree-node 4 (list (tree-node 1 nil) (tree-node 5 (list (tree-node 2 nil) (tree-node 3 nil) (tree-node 1 (list (tree-node 3 nil) (tree-node 6 nil))))) (tree-node 9 (list (tree-node 2 nil) (tree-node 11 nil))))))
+
+(defun tree-node (val children)
+    (cons 'tree (cons val children)))
+
+(defun binary-tree-node (val left-child right-child)
+    (list 'binary-tree val left-child right-child))
+
+(defun left-child (node)
+    (caddr node))
+
+(defun right-child (node)
+    (cadddr node))
+
+(defun tree-type (node)
+    (car node))
+
+(defun treep (node)
+    (eql (tree-type node) 'tree))
+
+(defun binary-treep (node)
+    (eql (tree-type node) 'binary-tree))
+
+(defun node-value (node)
+    (cadr node))
+
+(defun node-children (node)
+    (cond ((treep node) (cddr node))
+          ((binary-treep node) (remove nil (cddr node)))
+          (t (error "Unknown tree type."))))
+
+(defun tree-find (val node)
+    (cond ((null node) nil)
+          ((= val (node-value node)) val)
+          (t (tree-find-multi val (node-children node)))))
+
+(defun tree-find-multi (val nodes)
+    (if (null nodes)
+         nil 
+        (append (tree-find val (car nodes)) (tree-find-multi val (cdr nodes)))))
