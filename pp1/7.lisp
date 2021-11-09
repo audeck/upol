4fb1359421c89a14e30cbe67d3e5757b53b5b720
audeck <55060124+audeck@users.noreply.github.com>
2021-11-09 11:43:10 +0100

    Update 7.lisp

--- pp1/7.lisp
+++ pp1/7.lisp
@@ -1,19 +1,7 @@
-(defun plist-remove (plist prop)
-    (cond ((null plist) nil)
-          ((eql (car plist) prop) (plist-remove (cddr plist) prop))
-          (t (cons (car plist) (cons (cadr plist) (plist-remove (cddr plist) prop))))))
-
-(defun plist-add (plist prop val)
-    (cond ((null plist) (cons prop (cons val nil)))
-          ((eql (car plist) prop) (cons prop (cons val (cddr plist))))
-          (t (cons (car plist) (cons (cadr plist) (plist-add (cddr plist) prop val))))))
-
-// (tree-node 5 (list (tree-node 2 (list (tree-node 1 nil) (tree-node (tree-node 3 nil) (list (tree-node 4 nil))))) (tree-node 7 (list (tree-node 6 nil) (tree-node 8 nil)))))
-
-// (setf tree (tree-node 4 (list (tree-node 1 nil) (tree-node 5 (list (tree-node 2 nil) (tree-node 3 nil) (tree-node 1 (list (tree-node 3 nil) (tree-node 6 nil))))) (tree-node 9 (list (tree-node 2 nil) (tree-node 11 nil))))))
-
 (defun tree-node (val children)
     (cons 'tree (cons val children)))
+
+; (setf tree (tree-node 4 (list (tree-node 1 nil) (tree-node 5 (list (tree-node 2 nil) (tree-node 3 nil) (tree-node 1 (list (tree-node 3 nil) (tree-node 6 nil))))) (tree-node 9 (list (tree-node 2 nil) (tree-node 11 nil))))))
 
 (defun binary-tree-node (val left-child right-child)
     (list 'binary-tree val left-child right-child))
@@ -41,12 +29,26 @@
           ((binary-treep node) (remove nil (cddr node)))
           (t (error "Unknown tree type."))))
 
+;;; EOL
+
+(defun plist-remove (plist prop)
+    (cond ((null plist) nil)
+          ((eql (car plist) prop) (plist-remove (cddr plist) prop))
+          (t (cons (car plist) (cons (cadr plist) (plist-remove (cddr plist) prop))))))
+
+(defun plist-add (plist prop val)
+    (cond ((null plist) (cons prop (cons val nil)))
+          ((eql (car plist) prop) (cons prop (cons val (cddr plist))))
+          (t (cons (car plist) (cons (cadr plist) (plist-add (cddr plist) prop val))))))
+
+; (tree-node 5 (list (tree-node 2 (list (tree-node 1 nil) (tree-node (tree-node 3 nil) (list (tree-node 4 nil))))) (tree-node 7 (list (tree-node 6 nil) (tree-node 8 nil)))))
+
 (defun tree-find (val node)
     (cond ((null node) nil)
-          ((= val (node-value node)) val)
-          (t (tree-find-multi val (node-children node)))))
+          ((eql val (node-value node)) val)
+          (t (tree-find-children val (node-children node)))))
 
-(defun tree-find-multi (val nodes)
+(defun tree-find-children (val nodes)
     (cond ((null nodes) nil)
-          ((= val (tree-find val (car nodes)) val))
-          (t (tree-find-multi val (cdr nodes)))))
+          ((eql val (tree-find val (car nodes))) val) 
+          (t (tree-find-children val (cdr nodes)))))
