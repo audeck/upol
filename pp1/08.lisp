f3d259c31e4eb7673ef35fcd3674523e689bb5be
audeck <55060124+audeck@users.noreply.github.com>
2022-01-17 21:48:28 +0100

    Update 08.lisp

--- pp1/08.lisp
+++ pp1/08.lisp
@@ -1,42 +1,50 @@
-(defun my-member (x list)
-    (cond ((null list) nil)
-          ((eql x (car list)) (cons (car list) (cdr list)))
-          (t (my-member x (cdr list)))))
+(defun foldr (fun list init)
+  (if (null list)
+       init
+      (funcall fun (car list) (foldr fun (cdr list) init))))
 
-(defun my-member-higher (x list test)
-    (cond ((null list) nil)
-          ((funcall test x (car list)) (cons (car list) (cdr list)))
-          (t (my-member-higher x (cdr list) test))))
+;;; End-Of-Lecture
+
+(defun my-member (elem list)
+  (cond ((null list) nil)
+        ((eql elem (car list)) list)
+        (t (my-member elem (cdr list)))))
+
+(defun my-member-if (elem list fun)
+  (cond ((null list) nil)
+        ((funcall fun elem (car list)) list)
+        (t (my-member-if elem (cdr list) fun))))
 
 (defun my-mapcar (fun list)
-    (if (null list)
-        '()
-        (cons (funcall fun (car list)) 
-              (my-mapcar fun (cdr list)))))
-
-(defun foldr (fun list init)
-    (if (null list)
-        init
-        (funcall fun (car list) (foldr fun (cdr list) init))))
+  (labels ((help (elem rest)
+              (cons (funcall fun elem) rest)))
+    (foldr #'help list '())))
 
 (defun foldl (fun list init)
-    (labels ((iter (list ir) 
-                (if (null list) 
-                    ir 
-                    (iter (cdr list) (funcall fun ir (car list))))))
-        (iter list init)))
+  (labels ((iter (list ir) 
+              (if (null list) 
+                   ir 
+                  (iter (cdr list) (funcall fun ir (car list))))))
+    (iter list init)))
+
+(defun arithmetic-mean (a &rest rest)
+  (/ (foldr #'+ rest a) (1+ (length rest))))
+
+(defun equal-lists-p (list &rest lists)
+  (labels ((truth-list-p (list)
+              (if (null list)
+                   t
+                  (and (car list) (truth-list-p (cdr list))))))
+    (truth-list-p (apply #'mapcar #'eql list lists))))
+
+(defun my-mapcar (fun list &rest lists)
+  (labels ((help (fun list)
+               (if (null list)
+                   '()
+                   (cons (funcall fun (car list))
+                         (my-mapcar fun (cdr list))))))
+    (apply #'help fun list lists)))
 
 (defun my-length (list)
     (labels ((inc (elem rest) (+ 1 rest)))
         (foldr #'inc list 0)))
-
-(defun my-mapcar-foldr (fun list)
-    (labels ((HELP (elem rest)
-                 (cons (funcall fun elem) rest)))
-        (foldr #'HELP list '())))
-
-(defun arithmetic-mean (&rest numbers)
-    (/ (foldr #'+ numbers 0) (length numbers)))
-
-
-    
