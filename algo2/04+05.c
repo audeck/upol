bef6ffd6088a66d6495d12265eda5db9edf4f319
Audeck <55060124+Audeck@users.noreply.github.com>
2022-03-21 16:09:32 +0100

    Update 04+05.c

--- algo2/04+05.c
+++ algo2/04+05.c
@@ -1,37 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+/* TODO: malloc() success checking */
+/* TODO: TODO */
+
+#define max(a, b) ((a > b) ? a : b)
 
 /* Tree & node usage in this file is limited to binary search trees */
 typedef struct node {
     int data;
-    node* left;
-    node* right;
-    node* parent;
+    struct node* left;
+    struct node* right;
+    struct node* parent;
 } node;
 
 typedef struct tree {
     node* root;
 } tree;
 
+/* A 'node' wrapper for usage with 'queue' */
 typedef struct qnode {
-    int data;
-    node* prev;
-    node* next;
+    node* node;
+    struct qnode* twrd_first;
+    struct qnode* twrd_last;
 } qnode;
 
+/* O(1) queue 8) */
 typedef struct queue {
     qnode* first;
     qnode* last;
 } queue;
 
+
+/* Adds a new node containing data 'data' to binary search tree 't' */
 void add(tree *t, int data) {
-    node* new = malloc(sizeof(node));
+    node* new = (node*) malloc(sizeof(node));
     new->data = data;
     new->left = NULL;
     new->right = NULL;
     new->parent = NULL;
 
-    node* y;
+    node* y = NULL;
     node* x = t->root;
 
     while (x != NULL) {
@@ -42,7 +52,7 @@ void add(tree *t, int data) {
             x = x->right;
         }
     }
-
+    
     new->parent = y;
 
     if (y == NULL) {
@@ -54,30 +64,20 @@ void add(tree *t, int data) {
     }
 }
 
-void _print_in_order(node* n) {
-    if (n != NULL) {
-        _print_in_order(n->left);
-        printf("%i, ", n->data);
-        _print_in_order(n->right);
-    }
-}
-
-void print_in_order(tree t) {
-    _print_in_order(t.root);
-}
-
 int _depth(node* n) {
     if (n == NULL) {
         return 0;
     } else {
-        return 1 + max(_depth(n->left), _depth(n->right));
+        return (1 + max(_depth(n->left), _depth(n->right)));
     }
 }
 
+/* Returns the depth of a (general) tree */
 int depth(tree t) {
     return _depth(t.root);
 }
 
+/* Returns the maximum value in a sub-tree with root 'node' */
 int tree_max(node *root) {
     node* ptr = root;
 
@@ -88,6 +88,7 @@ int tree_max(node *root) {
     return ptr->data;
 }
 
+/* Returns the minimum value in a sub-tree with root 'node' */
 int tree_min(node *root) {
     node* ptr = root;
 
@@ -98,11 +99,12 @@ int tree_min(node *root) {
     return ptr->data;
 }
 
+/* Removes node containing 'data' */
 int tree_remove(tree *t, int data) {
-    node* cur = t->root;  // root could be NULL?
-
     /* If tree is empty */
-    if (cur == NULL) return 0;
+    if (t == NULL || t->root == NULL) return 0;
+
+    node* cur = t->root;  // Current node pointer
 
     /* Find 'data' node */
     while (cur->data != data) {
@@ -173,42 +175,233 @@ int tree_remove(tree *t, int data) {
 
     /* Case 3: 'cur' has both children (in place for style points) */
     if (cur->right != NULL && cur->left != NULL) {
+        /* Find left sub-tree maximum */
         int left_max = tree_max(cur->left);
 
+        /* Remove 'left_max' from tree & error check just in case */
         if (!tree_remove(t, left_max)) {
-            fprintf(stderr, "Something went wrong!");
+            fprintf(stderr, "[EXCEPTION @ tree_remove(tree*, int)] Something went terribly wrong\n");
         }
 
+        /* Swap in 'left_max' to current node's data */
         cur->data = left_max;
         return 1;
     }
 
     /* ??? */
+    fprintf(stderr, "[EXCEPTION @ tree_remove(tree*, int)] Reached (what should be) an unreachable return\n");
     return 0;
 }
 
-void _bft_enqueue(node* n, queue* q) {
-    if (n != NULL) {
-        qnode* qnode = malloc(sizeof(qnode));
-        qnode->data = n->data;
-        qnode->prev = q->last;
-        q->last = qnode;
+/* Crates a qnode and returns it's pointer (O(1) queue) */
+qnode* create_qnode(node* n) {
+    qnode* ptr = malloc(sizeof(qnode));
+    ptr->twrd_first = NULL;
+    ptr->twrd_last = NULL;
+    ptr->node = n;
+
+    return ptr;
+}
+
+/* Crates a queue and returns it's pointer (O(1) queue) */
+queue* create_queue() {
+    queue* ptr = malloc(sizeof(queue));
+    ptr->first = NULL;
+    ptr->last = NULL;
+
+    return ptr;
+}
+
+/* Enqueues a new qnode with qnode->node = n */
+void enqueue(queue* q, node* n) {
+    /* Create qnode */
+    qnode* qn = create_qnode(n);
+
+    /* If queue is empty */
+    if (q->first == NULL) {
+        q->first = qn;
+        q->last = qn;
+    } else {
+        qnode* prev_last = q->last;
+        prev_last->twrd_last = qn;
+        qn->twrd_first = prev_last;
+        q->last = qn;
     }
 }
 
+/* Dequeues a qnode and returns qnode->node->data */
+int dequeue(queue* q) {
+    if (q->first != NULL) {
+        qnode* popped = q->first;
+        int popped_data = popped->node->data;
+
+        q->first = popped->twrd_last;  // Don't have to NULL q->last with right enqueue
+        
+        free(popped);
+        return popped_data;
+    }
+
+    fprintf(stderr, "[EXCEPTION @ dequeue(queue*)] Dequeued from an empty queue (returned 0)\n");
+    return 0;
+}
+
+/* Private print_in_order() helper */
+void _print_in_order(node* n) {
+    if (n != NULL) {
+        _print_in_order(n->left);
+        printf("%i ", n->data);
+        _print_in_order(n->right);
+    }
+}
+
+/* Prints out the values in a tree ordered from lowest to highest to stdout */
+void print_in_order(tree t) {
+    _print_in_order(t.root);
+    printf("\n");
+}
+
+/* Prints tree 't' breadth-first to stdout */
 void print_bft(tree *t) {
     if (t->root != NULL) {
-        queue* print_order = malloc(sizeof(queue));
-        print_order->first = NULL;
-        print_order->last = NULL;
+        /* Create queue */
+        queue* print_queue = create_queue();
 
-        _bft_enqueue(t->root, print_order);
+        /* Enqueue root node */
+        enqueue(print_queue, t->root);
+
+        while (print_queue->first != NULL) {
+            qnode* first = print_queue->first;
+
+            /* Enqueue children */
+            if (first->node->left != NULL) {
+                enqueue(print_queue, first->node->left);
+            }
+
+            if (first->node->right != NULL) {
+                enqueue(print_queue, first->node->right);
+            }
+
+            /* Dequeue and print 'first' */
+            printf("%i ", dequeue(print_queue));
+        }
+
+        printf("\n");
+    }
+}
+
+/* Prints 'string' to stdout 'times' times */
+void print_times(char* string, int times) {
+    for (int i = 0; i < times; i += 1) {
+        printf("%s", string);
+    }
+}
+
+/* Prints BST(!) 't' to stdout in a structured manner (mono-space font only) */
+void print_structured(tree* t) {
+    if (t == NULL) return;
+
+    int height = depth(*t);  // Height of tree
+    int max_digits = log10(tree_max(t->root)) + 1;
+    int line_width = (pow(2, height) - 1);  // Max output line width
+
+    /* Create print queue */
+    queue* print_queue = create_queue();
+
+    /* Enqueue root (could be NULL) */
+    enqueue(print_queue, t->root);
+
+    /* Print the tree sctructure level by level */
+    for (int i = 0; i < height; i += 1) {
+        /* Print leading spaces */
+        print_times(" ", max_digits * (pow(2, height - 1 - i) - 1));
+
+        /* Print tree */
+        for (int j = 0; j < pow(2, i); j += 1) {
+            qnode* first = print_queue->first;
+            
+            /* If qnode->node is NULL, then it is an empty "filler" qnode (for formatting) */
+            if (first->node == NULL) {
+                /* Enqueue filler qnodes */
+                enqueue(print_queue, NULL);
+                enqueue(print_queue, NULL);
+
+                /* Print "empty" qnodes */
+                print_times("X", max_digits);
+                print_times(" ", max_digits * (int) (pow(2, height - i) - 1));
+
+                /* Move first pointer without dequeuing */
+                print_queue->first = print_queue->first->twrd_last;
+            } else {
+                /* Enqueue children */
+                enqueue(print_queue, first->node->left);
+                enqueue(print_queue, first->node->right);
+
+                /* Dequeue and print 'first' */
+                int num = dequeue(print_queue);
+                int num_digits = (num == 0) ? 1 : (int) (log10(abs(num)) + 1);
+                print_times("0", max_digits - num_digits);
+                printf("%i", num);
+                print_times(" ", max_digits * (int) (pow(2, height - i) - 1));
+            }
+        }
+
+        /* Print a trailing new-line */
+        printf("\n");
     }
 }
 
 
 
 int main(void) {
-    /* TODO: Tests */
-    return 0;
+    // tree* bst = malloc(sizeof(tree));
+    // bst->root = NULL;
+
+    // add(bst, 7);
+    // add(bst, 3);
+    // add(bst, 1);
+    // add(bst, 5);
+    // add(bst, 0);
+    // add(bst, 2);
+    // add(bst, 4);
+    // add(bst, 6);
+    // add(bst, 11);
+    // add(bst, 9);
+    // add(bst, 8);
+    // add(bst, 10);
+    // add(bst, 13);
+    // add(bst, 12);
+    // add(bst, 14);
+
+    // /*
+    //                   07
+    //           03              11
+    //       01      05      09      13
+    //     00  02  04  06  08  10  12  14
+    // */
+
+    // print_in_order(*bst);
+    // print_bft(bst);
+    // printf("\n");
+
+    // print_structured(bst);
+    // printf("\n");
+
+    // printf("Tree min: %i\n", tree_min(bst->root));
+    // printf("Tree max: %i\n", tree_max(bst->root));
+    // printf("Tree max left: %i\n", tree_max(bst->root->left));
+    // printf("\n");
+
+    // tree_remove(bst, 12);
+    // print_structured(bst);
+    // printf("\n");
+
+    // tree_remove(bst, 13);
+    // print_structured(bst);
+    // printf("\n");
+
+    // tree_remove(bst, 7);
+    // print_structured(bst);
+    // printf("\n");
+
+    // return 0;
 }
