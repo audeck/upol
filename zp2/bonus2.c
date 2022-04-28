1c330eaef951c265ec25af177154e7b567f16377
Audeck <55060124+Audeck@users.noreply.github.com>
2022-04-28 20:15:41 +0200

    Update bonus2.c

--- zp2/bonus2.c
+++ zp2/bonus2.c
@@ -4,6 +4,9 @@
 #define SIRKA 4
 #define VYSKA 5
 
+/* Should be noted that ##__VA_ARGS__ isn't standard C, but makes the macro more readable */
+#define VYTVOR(x, y, ...) (vytvor(x, y, ##__VA_ARGS__, 0));
+
 typedef struct obdelnik {
     double x;
     double y;
@@ -11,43 +14,54 @@ typedef struct obdelnik {
     double vyska;
 } obdelnik;
 
+/* Vytvori obdelnik */
 obdelnik vytvor(double x, double y, ...) {
-    obdelnik o = {x, y};
+    obdelnik rect = {x, y};
 
     va_list args;
     va_start(args, y);
 
-    if ((o.sirka = va_arg(args, double)) == 0) {
-        printf("Sirka & vyska == 0!\n");
-        o.sirka = SIRKA;
-        o.vyska = VYSKA;
+    rect.sirka = va_arg(args, double);  // Argument type can be int or double
+
+    if (rect.sirka == 0) {
+        /* Default sirka, vyska values */
+        rect.sirka = SIRKA;
+        rect.vyska = VYSKA;
+
+        /* End early */
+        va_end(args);
+        return rect;
     }
-    else if ((o.vyska = va_arg(args, double)) == 0) {
-        printf("Vyska == 0!\n");
-        printf("%lf\n", o.sirka);
-        o.vyska = o.sirka;
+
+    rect.vyska = va_arg(args, double);
+
+    if (rect.vyska == 0) {
+        /* Rectangle is square */
+        rect.vyska = rect.sirka;
     }
 
     va_end(args);
-
-    return o;
+    return rect;
 }
 
+/* Vypise obdelnik */
 void vypis(obdelnik rect) {
     printf("Obdelnik:\n");
-    printf(" - [%.0lf, %.0lf] [%.0lf, %.0lf]\n", rect.x, rect.y + rect.vyska, rect.x + rect.sirka, rect.y + rect.vyska);
-    printf(" - [%.0lf, %.0lf] [%.0lf, %.0lf]\n", rect.x, rect.y, rect.x + rect.sirka, rect.y);
+    printf(" - [%.0lf, %.0lf]", rect.x,              rect.y + rect.vyska);
+    printf(" [%.0lf, %.0lf]\n", rect.x + rect.sirka, rect.y + rect.vyska);
+    printf(" - [%.0lf, %.0lf]", rect.x,              rect.y);
+    printf(" [%.0lf, %.0lf]\n", rect.x + rect.sirka, rect.y);
 }
 
 
 
 int main(void) {
-    obdelnik def = vytvor(0, 0, 0);
-    vypis(def);
-    obdelnik square  = vytvor(1, 1, 3, 0);
-    vypis(square);
-    obdelnik rectangle = vytvor(-1, -1, 6, 5, 0);
-    vypis(rectangle);
+    // obdelnik dflt = VYTVOR(0.0, 0.0);
+    // vypis(dflt);
+    // obdelnik square = VYTVOR(1.0, 1.0, 3.0);
+    // vypis(square);
+    // obdelnik rectangle = VYTVOR(-1.0, -1.0, 6.0, 5.0);
+    // vypis(rectangle);
 
     return 0;
 }
