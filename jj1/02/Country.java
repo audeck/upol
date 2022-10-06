5b3da21beea2765fdf3e2aa81dee04054c1b4e3f
audeck <55060124+audeck@users.noreply.github.com>
2022-10-06 16:02:16 +0200

    Adapted Google's Java Style

--- jj1/02/Country.java
+++ jj1/02/Country.java
@@ -1,16 +1,16 @@
 package cz.upol.jj1;
 
 class Country {
-    String name;
-    String phoneCode;
+  String name;
+  String phoneCode;
 
-    Country(String name, String phoneCode) {
-        this.name = name;
-        this.phoneCode = phoneCode;
-    }
+  Country(String name, String phoneCode) {
+    this.name = name;
+    this.phoneCode = phoneCode;
+  }
 
-    void print() {
-        String output = "Stát{jméno=" + this.name + ", předvolba=" + this.phoneCode +"}";
-        System.out.println(output);
-    }
+  void print() {
+    String output = "Stát{jméno=" + this.name + ", předvolba=" + this.phoneCode +"}";
+    System.out.println(output);
+  }
 }
