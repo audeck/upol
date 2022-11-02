fa8abfa8ebe090fcb04423cef30ea8a06e006b37
audeck <55060124+audeck@users.noreply.github.com>
2022-11-02 21:44:19 +0100

    Javadoc'd

--- jj1/02/Index.java
+++ jj1/02/Index.java
@@ -1,7 +1,10 @@
 package cz.upol.jj1;
 
+/** This class implements an index, which contains arrays of people and countries */
 class Index {
+  /** Array of people in this index */
   Person[] persons;
+  /** Array of countries in this index */
   Country[] countries;
 
   Index(Person[] persons, Country[] countries) {
@@ -9,7 +12,7 @@ class Index {
     this.countries = countries;
   }
 
-  // Returns the number of people in living in "countryName"
+  /** Returns the number of people in living in a given country. */
   int count(String countryName) {
     int count = 0;
 
@@ -22,7 +25,7 @@ class Index {
     return count;
   }
 
-  // Returns the number of people named "firstName lastName"
+  /** Returns the number of people of given first name and last name. */
   int count(String firstName, String lastName) {
     int count = 0;
 
@@ -35,7 +38,11 @@ class Index {
     return count;
   }
 
-  // Returns the person with a given id (returns null if not found)
+  /**
+   * Returns the person of a given id.
+   *
+   * @return {@code null} if not found, otherwise the found person
+   */
   Person search(int id) {
     for (Person person : this.persons) {
       if (person.id == id) {
@@ -45,7 +52,9 @@ class Index {
     return null;
   }
 
-  // Returns an array of all people living in "countryName"
+  /**
+   * @return an array of people living in a given country
+   */
   Person[] search(String countryName) {
     Person[] output = new Person[count(countryName)];
     int outputIndex = 0;
@@ -60,7 +69,9 @@ class Index {
     return output;
   }
 
-    // Returns an array of all people named "firstName lastName"
+  /**
+   * @return an array of people of given first name and last name
+   */
   Person[] search(String firstName, String lastName) {
     Person[] output = new Person[count(firstName, lastName)];
     int outputIndex = 0;
@@ -75,7 +86,13 @@ class Index {
     return output;
   }
 
-  // Sets a person's country to a country given by phone code
+  /**
+   * Sets a person's country to a country given by phone code.
+   *
+   * @param person person whose country to set
+   * @param phoneCode phone code to set country by
+   * @return {@code true} if country is set successfully, otherwise {@code false}
+   */
   boolean setCountryByPhoneCode(Person person, String phoneCode) {
     for (Country country : this.countries) {
       if (country.phoneCode.equals(phoneCode)) {
@@ -87,7 +104,7 @@ class Index {
     return false;
   }
 
-  // Prints out the default index string
+  /** Prints out the default index string */
   void print() {
     StringBuilder output = new StringBuilder("Rejstřík{ ");
 
