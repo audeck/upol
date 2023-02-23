48ff03cea24c11e79024344b9fe16629c6b5ab7b
audeck <55060124+audeck@users.noreply.github.com>
2023-02-23 14:47:13 +0100

    Create bits.h

---
+++ os1/02/bits.h
@@ -0,0 +1,10 @@
+#ifnded BITS_H
+#define BITS_H
+
+/* Decodes the binary representation of a number from bits and returns it */
+int bits2int(char* bits);
+
+/* Encodes 'num' into its binary representation and stores it in 'bits' */
+void int2bits(char* bits, int num);
+
+#endif
