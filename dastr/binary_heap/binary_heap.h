de2dd6499cfa84023cb7a5d83d3bbdd29664824f
audeck <55060124+audeck@users.noreply.github.com>
2023-04-21 01:32:24 +0200

    Removed private function declarations

--- dastr/binary_heap/binary_heap.h
+++ dastr/binary_heap/binary_heap.h
@@ -10,9 +10,6 @@ typedef struct {
 binary_heap* make_binary_heap();
 void destruct(binary_heap* heap);
 void print(binary_heap* heap);
-void realloc_data(binary_heap* heap, size_t new_limit);
-void expand_data_if_needed(binary_heap* heap);
-void shrink_data_if_needed(binary_heap* heap);
 void swap_keys(binary_heap* heap, size_t index1, size_t index2);
 void set_key(binary_heap* heap, size_t index, int key);
 void insert(binary_heap* heap, int key);
